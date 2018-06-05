82d6e8d1d9a49c51b5503bdfdc889c0c1dbc9430
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-05 01:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine(katzDeliLine) {
 
   for (let i = 0; i < katzDeliLine.length; i++) {
   
-  array.push(`${i+1}. ${katzDeliLine(i)}`)}
+  array.push(i + 1, ". " katzDeliLine[i])
   
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
