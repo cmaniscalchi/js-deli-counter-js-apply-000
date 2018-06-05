37c6d48d1de13f5ba612f7426d7c180f4ee3b8de
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-05 00:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, person) {
+  katzDeliLine.push(person);
   return `Welcome, ${person}. You are number ${katzDeliLine.length + 1} in line.`;
 }
